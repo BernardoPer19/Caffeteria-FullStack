5442f19404d5c20074af3a7948c1a62ac77776f3
jhoaking <wawxper08@gmail.com>
2025-06-07 11:38:47 -1100

    añadiendo cambios para produccion

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: import.meta.env.BASE_URL || "http://localhost:3000", // Usa la variable de entorno o localhost por defecto
+  baseURL: "https://backend-cafeteria-hopg.onrender.com",
   withCredentials: true,
 });
 
